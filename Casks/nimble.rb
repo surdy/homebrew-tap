b0a17c6f81eec0eb0c3b3240c69a9168dab8c7be
surdy <surdy@users.noreply.github.com>
2026-03-23 02:25:12 -0700

    fix: convert nimble from Formula to Cask for .dmg support

---
+++ Casks/nimble.rb
@@ -0,0 +1,17 @@
+cask "nimble" do
+  version "0.1.0"
+  sha256 "PLACEHOLDER_MACOS_SHA256"
+
+  url "https://github.com/surdy/nimble/releases/download/v#{version}/Nimble_#{version}_aarch64.dmg",
+      verified: "github.com/surdy/nimble/"
+  name "Nimble"
+  desc "A keyboard-driven command palette for your desktop"
+  homepage "https://github.com/surdy/nimble"
+
+  app "Nimble.app"
+
+  zap trash: [
+    "~/Library/Application Support/Nimble",
+    "~/Library/Preferences/com.nimble.app.plist",
+  ]
+end
